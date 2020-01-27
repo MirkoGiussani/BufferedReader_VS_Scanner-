6afe4ff3bf1536880b66d2c66615730fc3d15ac4
giussani.mirko <giussani.mirko@BIBLIO-PC06.OSDBSESTO.local>
2020-01-27 13:11:32 +0100

    create class "Creazione file"

---
+++ BufferedReader_VS_Scanner/src/bufferedreader_vs_scanner/BufferedReader_VS_Scanner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bufferedreader_vs_scanner;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author giussani.mirko
+ */
+public class BufferedReader_VS_Scanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        CreazioneFile file = new CreazioneFile();
+        
+        file.creazioneFile();
+    }
+    
+}
---
+++ BufferedReader_VS_Scanner/src/bufferedreader_vs_scanner/CreazioneFile.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bufferedreader_vs_scanner;
+
+/**
+ *
+ * @author giussani.mirko
+ */
+import java.io.File;
+import java.io.FileWriter;  //ci consente di riempire un file
+import java.io.IOException;
+import java.util.*;
+
+public class CreazioneFile {
+    public void creazioneFile() throws IOException{
+        File myFile = new File("File.txt");
+        if(myFile.exists()){ //se esiste il file lo elimino 
+            myFile.delete();
+        } 
+        
+        myFile.createNewFile();
+
+        FileWriter myFileWriter = new FileWriter(myFile);
+        int a = 0; // numero minimo
+        int b = 256; // numero massimo
+        int c = ((b-a) + 1);
+        Random r = new Random();
+        int casuale;
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 5; j++) {
+                 casuale = (int) r.nextInt(c) + a;
+                 myFileWriter.write(casuale + " ");
+            }
+            myFileWriter.write(System.getProperty("line.separator"));
+        }
+        myFileWriter.close();
+    }
+}
