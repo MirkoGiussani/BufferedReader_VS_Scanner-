a2b3c367df4860c696e3aa650dc0f9762d9ad3ef
Mirko <Mirko@192.168.1.6>
2020-01-29 16:41:28 +0100

    add a reading file

--- BufferedReader_VS_Scanner/src/bufferedreader_vs_scanner/Lettura_PopolazioneArray.java
+++ BufferedReader_VS_Scanner/src/bufferedreader_vs_scanner/Lettura_PopolazioneArray.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bufferedreader_vs_scanner;
 
 import java.io.File;
@@ -10,16 +5,42 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- * 
+ *
  * @author giussani.mirko
  */
 public class Lettura_PopolazioneArray {
-    public void letturaFile() throws FileNotFoundException{
-        File myFile = new File("File.txt"); //selezionare il percorso da importare  
-        Scanner myReader = new Scanner(myFile);
-        int conteggio;
+
+    public void letturaFile() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("File.txt"));
+
+        int righe = 0;
+        int colonne = 0;    //questi numeri non servono a nulla
+
+        while (input.hasNextLine()) {
+            colonne = 0;    //nel file è presente uno spazio finale quindi si parte dal 0 e non da 1
+            righe++;
+            String riga = input.nextLine();
+            for (int i = 0; i < riga.length(); i++) {
+                if (riga.charAt(i) == ' ') {
+                    colonne++;
+                }
+            }
+        }
+
+        System.out.println("colonne: " + colonne + " righe: " + righe);
+        int[] array = new int[500000];
+
+        input = new Scanner(new File("File.txt"));
         
+        for (int i = 0; i < array.length; i++) {
+            if (input.hasNextInt()) {
+                array[i] = input.nextInt();
+            }
+        }
         
-        
+        //stampa array
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i+1) + " numero: " + array[i]);
+        }
     }
 }
